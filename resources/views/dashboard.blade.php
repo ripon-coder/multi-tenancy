0af046a727f74268f1012b231a5d360d9de54053
Ripon Shikder <riponoffice121@gmail.com>
2023-10-17 14:52:25 +0600

    working on tenant

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <a href="your-link-here">
+                        <x-primary-button>{{ __('Create Tenant') }}</x-primary-button>
+                    </a>
                 </div>
             </div>
         </div>
